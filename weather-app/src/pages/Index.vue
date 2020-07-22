6fa6996100fec6153ac2655e29da2ff47e619a51
Anna <wrobel.anne@outlook.com>
2020-07-22 10:52:48 +0200

    finished app

--- weather-app/src/pages/Index.vue
+++ weather-app/src/pages/Index.vue
@@ -1,13 +1,19 @@
 <template>
-  <q-page class="flex column">
+  <q-page class="flex column" :class="bgClass">
     <div class="col q-pt-lg q-px-md">
-      <q-input v-model="text" placeholder="Search" dark borderless>
+      <q-input
+        v-model="search"
+        @keyup.enter="getWeatherBySearch"
+        placeholder="Search"
+        dark
+        borderless
+      >
         <template v-slot:before>
           <q-icon @click="getLocation" name="my_location" />
         </template>
 
         <template v-slot:append>
-          <q-btn round dense flat icon="search" />
+          <q-btn @click="getWeatherBySearch" round dense flat icon="search" />
         </template>
       </q-input>
     </div>
@@ -20,12 +26,12 @@
 
         <div class="text-h1 text-weight-thin q-my-lg relative-position">
           <span>{{Math.round(weatherData.main.temp)}}</span>
-          <span class="text-h4 degree relative-position">&deg;</span>
+          <span class="text-h4 degree relative-position">&deg;C</span>
         </div>
       </div>
 
       <div class="col text-center">
-        <img :src="`http://openweathermap.org/img/wn/01d@2x.png`" />
+        <img :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`" />
       </div>
     </template>
 
@@ -35,7 +41,7 @@
           Weather
           <br />App
         </div>
-        <q-btn class="col" flat @click="getLocation">
+        <q-btn class="col q-pt-lg" flat @click="getLocation">
           <q-icon left size="3em" name="my_location" />
           <div>Find my location</div>
         </q-btn>
@@ -59,20 +65,44 @@ export default {
       apiKey: "856777ea40e28721fef25105b39164db"
     };
   },
+  computed: {
+    bgClass() {
+      if (this.weatherData) {
+        if (this.weatherData.weather[0].icon.endsWith("n")) {
+          return "bg-night";
+        } else {
+          return "bg-day";
+        }
+      }
+      return "";
+    }
+  },
   methods: {
     getLocation() {
+      this.$q.loading.show();
       navigator.geolocation.getCurrentPosition(position => {
-        console.log("position: ", position);
         this.lat = position.coords.latitude;
         this.lon = position.coords.longitude;
         this.getWeatherByCoords();
+        this.$q.loading.hide();
       });
     },
     getWeatherByCoords() {
+      this.$q.loading.show();
       this.$axios(
-        `${this.apiUrl}?lat=${this.lat}&lon=${this.lon}9&appid=${this.apiKey}&units=metric`
+        `${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`
       ).then(response => {
         this.weatherData = response.data;
+        this.$q.loading.hide();
+      });
+    },
+    getWeatherBySearch() {
+      this.$q.loading.show();
+      this.$axios(
+        `${this.apiUrl}?q=${this.search}&appid=${this.apiKey}&units=metric`
+      ).then(response => {
+        this.weatherData = response.data;
+        this.$q.loading.hide();
       });
     }
   }
@@ -84,6 +114,12 @@ body {
 }
 .q-page {
   background: linear-gradient(to bottom, #007991, #78ffd6);
+  &.bg-night {
+    background: linear-gradient(to bottom, #000428, #004e92);
+  }
+  &.bg-day {
+    background: linear-gradient(to bottom, #2980b9, #6dd5fa, #ffffff);
+  }
 }
 .degree {
   top: -44px;
